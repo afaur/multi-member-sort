624702373bc1720beee53a1907cf1c0bbb867c09
Adam Faur <adam.faur@gmail.com>
2016-09-06 15:07:36 -0700

    Incremental commit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::io;
+use std::io::{Result, stdin, stdout};
 use std::io::prelude::*;
 use std::fs::File;
 
-fn read_line(input: &mut String) -> io::Result<()> {
-  try!(io::stdin().read_line(input));
+fn read_line(input: &mut String) -> Result<()> {
+  try!(stdin().read_line(input));
   Ok(())
 }
 
-fn read_file(filename: &'static str, file_contents: &mut String) -> io::Result<()> {
+fn read_file(filename: &'static str, file_contents: &mut String) -> Result<()> {
   let mut f = try!(File::open(filename));
   try!(f.read_to_string(file_contents));
   Ok(())
@@ -15,8 +15,12 @@ fn read_file(filename: &'static str, file_contents: &mut String) -> io::Result<(
 
 fn main() {
   let mut input = String::new();
+  print!("How many people? ");
+  stdout().flush().ok();
   read_line(&mut input).unwrap();
-  println!("{:?}", input);
+
+  let list_count = input.trim().parse::<i32>().ok().unwrap();
+  println!("{:?}", list_count);
 
   let mut file_contents = String::new();
   read_file("./data/list.txt", &mut file_contents).unwrap();

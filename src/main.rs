2143efbbe57556bfdbe62bf4e289e35f359d0986
Blaine Schmeisser <blainesch@gmail.com>
2016-09-06 15:50:32 -0700

    Make lists for each person sorting list.

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,30 @@ fn read_file(filename: &'static str, file_contents: &mut String) -> Result<()> {
   Ok(())
 }
 
-fn main() {
+fn get_list_count() -> i32 {
   let mut input = String::new();
   print!("How many people? ");
   stdout().flush().ok();
   read_line(&mut input).unwrap();
 
-  let list_count = input.trim().parse::<i32>().ok().unwrap();
-  println!("{:?}", list_count);
+  return input.trim().parse::<i32>().unwrap();
+}
 
+fn main() {
   let mut file_contents = String::new();
   read_file("./data/list.txt", &mut file_contents).unwrap();
-  println!("{:?}", file_contents.lines());
+  let original_list:Vec<&str> = file_contents.trim().split("\n").collect();
+  println!("{:?}", original_list);
+
+  let list_count = get_list_count();
+  let mut lists:Vec<Vec<&str>> = vec![];
+
+  for _ in 0..list_count {
+    let mut new_list = original_list.clone();
+    new_list.sort_by(|a, b| {
+      a.cmp(b)
+    });
+    lists.push(new_list);
+  }
+  println!("{:?}", lists);
 }

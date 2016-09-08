55e37fa0541a9e5e2a5b7adcc0dfcff1c752dd40
Blaine Schmeisser <blainesch@gmail.com>
2016-09-08 15:33:37 -0700

    Add a position test.

--- src/user_sorted_list.rs
+++ src/user_sorted_list.rs
@@ -16,4 +16,24 @@ impl UserSortedList {
   pub fn position(&self, needle: &String) -> usize {
     self.list.iter().position(|item| item == needle).unwrap()
   }
+
+}
+
+#[cfg(test)]
+mod user_sorted_list_test {
+  use super::*;
+
+  #[test]
+  fn test_position() {
+    let user_sorted_list = UserSortedList {
+      list: vec![
+        "tiny".to_string(),
+        "taco".to_string(),
+        "team".to_string()
+      ]
+    };
+    assert_eq!(0, user_sorted_list.position(&"tiny".to_string()));
+    assert_eq!(1, user_sorted_list.position(&"taco".to_string()));
+    assert_eq!(2, user_sorted_list.position(&"team".to_string()));
+  }
 }
